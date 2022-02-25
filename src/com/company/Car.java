be0ace293e43e4cafb3ed2a681f95830ac0e12e0
Dilbara Asanalieva <dilbara2025@gmail.com>
2022-02-25 15:37:00 +0600

    exception

---
+++ src/com/company/Car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Car implements AutoCloseable{
+    @Override
+    public void close() throws Exception {
+        System.out.println("Машина жабылып жатат");
+    }
+    public void drive(){
+        System.out.println("Машина журуп жатат");
+    }
+}
